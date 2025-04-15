1733d86a1c19afdd2a6114281db24266f1c08deb
Stadtk1nd <jbbreton@gmail.com>
2025-04-15 17:10:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ ALPHA_VANTAGE_API_KEY = os.environ.get("ALPHA_VANTAGE_API_KEY")
 LUNARCRUSH_API_KEY = os.environ.get("LUNARCRUSH_API_KEY")
 
 # Interface Streamlit
-st.title("Assistant de Trading Crypto v2")
+st.title("Assistant de Trading Crypto v3")
 st.write("Entrez les paramètres pour générer un plan de trading.")
 
 # Formulaire dynamique
